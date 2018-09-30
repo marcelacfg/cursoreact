2911b9beec963d900da2b1e64be11580995a7050
Marcela Gomes <marcelacfg@gmail.com>
2018-09-30 13:31:53 -0700

    project setup

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Hello World',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi!</h1>
+    <p>Teste.</p>
+    <Link to="/page-2/">Pagina 2</Link>
+  </div>
+)
+
+export default IndexPage
